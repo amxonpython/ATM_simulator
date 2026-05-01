ba43915bca6c8d78b13ed7f0530b329071ac253d
lampa <134073971+amxonpython@users.noreply.github.com>
2026-05-01 23:53:31 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
+    static int terminal_sym = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean working = true;
@@ -10,19 +12,35 @@ public class Main {
             String action = scanner.nextLine();
             switch (action){
                 case "replenish":
-                    ATM_API.replenish();
+                    System.out.println("сколько хотите полажить на счёт?");
+                    int sym = scanner.nextInt();
+                    ATM_API.replenish(sym);
+                    System.out.println("ваша текущий баланс: " + ATM_API.balance);
+                    String s = scanner.nextLine();
                     break;
                 case "take off":
-                    ATM_API.take_off();
+                    System.out.println("сколько хотите снять?");
+                    sym = scanner.nextInt();
+                    ATM_API.take_off(sym);
+                    if (ATM_API.itog == "true"){
+                        System.out.println("на вашем балансе: " + ATM_API.balance);
+                    }else {
+                        System.out.println("на вашем балансе недостаточно денег");
+                    }
+                    s = scanner.nextLine();
                     break;
                 case "find out the balance":
-                    ATM_API.find_out_the_balance();
+                    System.out.println("на вашем баналсе: " + ATM_API.balance);
                     break;
                 case "exit":
                     working = false;
                     break;
                 case "help":
-
+                    System.out.println("replenish - пополнить баланс");
+                    System.out.println("take off - снять бананс");
+                    System.out.println("find out the balance - узнать баланс");
+                    System.out.println(); //разделитель
+                    break;
                 default:
                     System.out.println("Error: неизвестная комманда");
             }
