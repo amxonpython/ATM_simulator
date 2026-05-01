ed71bd5ef27043a9e18a5358a15e322d4fd9fc17
lampa <134073971+amxonpython@users.noreply.github.com>
2026-05-01 23:54:08 +0300

    Update ATM_API.java

--- ATM_API.java
+++ ATM_API.java
@@ -3,34 +3,21 @@ import java.util.Scanner;
 public class ATM_API {
 
     static int balance = 0;
-    public static void replenish(){
+    static String itog = "null";
+
+    public static void replenish(int sym){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("сколько хотите полажить на счёт?");
-        int sym = scanner.nextInt();
         balance = (balance + sym);
-        System.out.println("ваша текущий баланс: " + balance);
 
     }
 
-    public static void take_off(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("сколько хотите снять?");
-        int sym = scanner.nextInt();
+    public static void take_off(int sym){
         if (sym > balance){
-            System.out.println("недостаточно денег");
+            itog = "false";
         }else {
             balance = (balance - sym);
-            System.out.println("на вашем балансе: " + balance);
+            itog = "true";
         }
-    }
-    public static void find_out_the_balance(){
-        System.out.println("на вашем баналсе: " + balance);
-    }
-    public static void help(){
-        System.out.println("replenish - пополнить баланс");
-        System.out.println("take off - снять бананс");
-        System.out.println("find out the balance - узнать баланс");
-        System.out.println(); //разделитель
     }
 }
